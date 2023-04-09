7deb2ab8fbdfcf4251f4b5714037a72a708118b4
thelazyindian <satyabrat.me@gmail.com>
2023-04-09 22:17:40 +0530

    Add Settings Page

--- lib/presentation/settings/settings_page.dart
+++ lib/presentation/settings/settings_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:saiyo_pets/presentation/common/views/app_empty_view.dart';
+import 'package:saiyo_pets/constants/dimens.dart';
 import 'package:saiyo_pets/presentation/settings/widgets/settings_app_bar.dart';
 
 class SettingsPage extends StatelessWidget {
@@ -11,14 +11,94 @@ class SettingsPage extends StatelessWidget {
 
     return Scaffold(
       appBar: const SettingsAppBar(),
-      extendBody: true,
-      extendBodyBehindAppBar: true,
       backgroundColor: colorScheme.background,
-      body: const Center(
-        child: AppEmptyView(
-          message: 'Something\'s missing here.',
-        ),
+      body: ListView(
+        padding: const EdgeInsets.all(16.0),
+        children: [
+          const _SettingsTile(
+            child: ListTile(
+              title: Text('Dark Mode'),
+              trailing: Switch(value: true, onChanged: null),
+            ),
+          ),
+          const _SettingsTile(
+            child: ListTile(
+              title: Text('Language'),
+              trailing: Text('English'),
+            ),
+          ),
+          const _SettingsTile(
+            child: ListTile(
+              title: Text('About'),
+              trailing: Icon(Icons.arrow_forward_ios),
+            ),
+          ),
+          const _SettingsTile(
+            child: ListTile(
+              title: Text('Terms of Service'),
+              trailing: Icon(Icons.arrow_forward_ios),
+            ),
+          ),
+          const _SettingsTile(
+            child: ListTile(
+              title: Text('Privacy Policy'),
+              trailing: Icon(Icons.arrow_forward_ios),
+            ),
+          ),
+          const _SettingsTile(
+            child: ListTile(
+              title: Text('Contact Us'),
+              trailing: Icon(Icons.arrow_forward_ios),
+            ),
+          ),
+          const _SettingsTile(
+            child: ListTile(
+              title: Text('Rate Us'),
+              trailing: Icon(Icons.arrow_forward_ios),
+            ),
+          ),
+          const _SettingsTile(
+            child: ListTile(
+              title: Text('Share'),
+              trailing: Icon(Icons.arrow_forward_ios),
+            ),
+          ),
+          const _SettingsTile(
+            child: ListTile(
+              title: Text('Log Out'),
+              trailing: Icon(Icons.arrow_forward_ios),
+            ),
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class _SettingsTile extends StatelessWidget {
+  const _SettingsTile({super.key, required this.child});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return Container(
+      margin: const EdgeInsets.only(bottom: 20.0),
+      decoration: BoxDecoration(
+        color: colorScheme.background,
+        borderRadius: Dimens.brc16,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.1),
+            spreadRadius: 2,
+            blurRadius: 2,
+            offset: const Offset(0, 2),
+          ),
+        ],
+      ),
+      child: child,
     );
   }
 }
